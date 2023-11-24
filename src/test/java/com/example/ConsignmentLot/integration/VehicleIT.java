f74541dcdcda0c8ad4bda7f6d3760d7a515f210d
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2023-11-24 16:28:15 +0000

    Added vehicle integrated test

---
+++ src/test/java/com/example/ConsignmentLot/integration/VehicleIT.java
@@ -0,0 +1,55 @@
+package com.example.ConsignmentLot.integration;
+
+import com.example.ConsignmentLot.entities.Vehicle;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Sql("classpath:test-data.sql")
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@TestPropertySource(properties = {"spring.sql.init.mode=never"})
+@ActiveProfiles("test")
+class VehicleIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    private final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+    @Test
+    void testGettingAllVehicles() throws Exception {
+        Vehicle[] actualPersons = getAllVehicles();
+
+        assertEquals(3, actualPersons.length);
+    }
+
+    private Vehicle[] getAllVehicles() throws Exception {
+        MvcResult result =
+                (this.mockMvc.perform(MockMvcRequestBuilders.get("/vehicles")))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                        .andReturn();
+
+        String contentAsJson = result.getResponse().getContentAsString();
+        return mapper.readValue(contentAsJson, Vehicle[].class);
+    }
+
+}
+
