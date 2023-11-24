78c9b390e2bbe89a50cad084581465a8ccb4ce3e
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2023-11-24 15:00:44 +0000

    Added vehicle controller

--- src/main/java/com/example/ConsignmentLot/controllers/VehicleController.java
+++ src/main/java/com/example/ConsignmentLot/controllers/VehicleController.java
@@ -1,4 +1,28 @@
 package com.example.ConsignmentLot.controllers;
 
+import com.example.ConsignmentLot.entities.Vehicle;
+import com.example.ConsignmentLot.services.IVehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vehicles")
 public class VehicleController {
+
+    private final IVehicleService service;
+
+    @Autowired
+    public VehicleController(IVehicleService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Vehicle> getAllVehicles() {
+        return service.getAllVehicles();
+    }
+
 }
---
+++ src/main/java/com/example/ConsignmentLot/services/IVehicleService.java
@@ -0,0 +1,9 @@
+package com.example.ConsignmentLot.services;
+
+import com.example.ConsignmentLot.entities.Vehicle;
+
+import java.util.List;
+
+public interface IVehicleService {
+    List<Vehicle> getAllVehicles();
+}
